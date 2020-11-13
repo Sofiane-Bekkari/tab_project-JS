17aa90f6f5318ab4fd1585560fbfd6fac96866a0
Sofiane <sofhill.ss@gmail.com>
2020-11-13 14:26:13 +0100

    added a js to tabs-btn

--- app.js
+++ app.js
@@ -2,3 +2,26 @@
 const currentYear = document.getElementById('date');
 const date = new Date().getFullYear();
 currentYear.innerHTML = date; 
+
+// set selected btns
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const artical = document.querySelectorAll('.content');
+
+about.addEventListener('click' , function(e){
+    const id = e.target.dataset.id
+    if (id){
+        // remove active-btn from btns
+        btns.forEach(function (btn){
+            btn.classList.remove("active-btn");
+            e.target.classList.add("active-btn");
+        });
+        // hide active content on articals
+        artical.forEach(function (content){
+            content.classList.remove("active");
+        });
+        const element = document.getElementById(id);
+        console.log(element)
+        element.classList.add("active");
+    }
+})
